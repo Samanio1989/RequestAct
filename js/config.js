e1d814aeb9fa7678816dbb09966747b1aa017c17
Samanio1989 <141902886+Samanio1989@users.noreply.github.com>
2026-07-10 12:47:52 +0700

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-window.REQUEST_ACT_CONFIG = Object.freeze({
-  API_URL: 'https://script.google.com/macros/s/AKfycbyW_Go6C-8U8RGtVAd-6q4DFYiEKVFS6SpKqdHvF4u0_Je2DRBaUNXi32pae9UouSTE/exec',
-  CHECKIN_URL: 'https://activity.aru.ac.th/',
-  PASSCODE_URL: 'http://sars.aru.ac.th/',
-  LINE_OA_URL: 'https://line.me/R/ti/p/@678bgmnu'
-});
